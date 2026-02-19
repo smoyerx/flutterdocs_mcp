394977be1208bc73104527f0a6ea0e1f0b7f9441
Steve Moyer <smoyerx@zylkr.dev>
2026-02-19 15:29:48 -0600

    load.py: separate entity/member types.

--- doc_gen/DOCDB_SCHEMA.sql
+++ doc_gen/DOCDB_SCHEMA.sql
@@ -3,7 +3,12 @@ CREATE TABLE identifier (
     name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE type (
+CREATE TABLE entity_type (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE member_type (
     id INTEGER PRIMARY KEY,
     name TEXT UNIQUE NOT NULL
 );
@@ -11,32 +16,32 @@ CREATE TABLE type (
 CREATE TABLE library (
     id INTEGER PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
-    content_markdown TEXT
+    content_markdown TEXT NOT NULL
 );
 
 CREATE TABLE entity (
     id INTEGER PRIMARY KEY,
     library_id INTEGER NOT NULL,
     identifier_id INTEGER NOT NULL,
-    type_id INTEGER NOT NULL,
-    content_markdown TEXT,
+    entity_type_id INTEGER NOT NULL,
+    content_markdown TEXT NOT NULL,
     snippet_markdown TEXT,
     UNIQUE(identifier_id, library_id),
     FOREIGN KEY (library_id) REFERENCES library(id),
     FOREIGN KEY (identifier_id) REFERENCES identifier(id),
-    FOREIGN KEY (type_id) REFERENCES type(id)
+    FOREIGN KEY (entity_type_id) REFERENCES entity_type(id)
 );
 
 CREATE TABLE member (
     id INTEGER PRIMARY KEY,
     entity_id INTEGER NOT NULL,
     identifier_id INTEGER NOT NULL,
-    type_id INTEGER NOT NULL,
-    content_markdown TEXT,
+    member_type_id INTEGER NOT NULL,
+    content_markdown TEXT NOT NULL,
     UNIQUE(identifier_id, entity_id),
     FOREIGN KEY (entity_id) REFERENCES entity(id),
     FOREIGN KEY (identifier_id) REFERENCES identifier(id),
-    FOREIGN KEY (type_id) REFERENCES type(id)
+    FOREIGN KEY (member_type_id) REFERENCES member_type(id)
 );
 
 CREATE INDEX idx_entity_identifier ON entity(identifier_id);
